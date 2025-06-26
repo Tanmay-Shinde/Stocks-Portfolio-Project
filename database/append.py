2736666b50dd694f7ed35d5559ee292ce89f0e49
Tanmay Shinde <tanmayshinde1401@gmail.com>
2025-06-26 05:49:12 -0400

    added code for appending new symbols data to db

---
+++ database/append.py
@@ -0,0 +1,21 @@
+from database.bronze import symbol_staging
+from database import connection
+
+
+def main():
+    success = False
+    engine = connection.get_engine()
+    file_path = input("Enter file path with new symbols data: ")
+    try:
+        success = symbol_staging.append(engine, file_path)
+    except Exception as e:
+        print(e)
+
+    if success:
+        print("Successfully appended to database")
+    else:
+        print("Failed to append to database")
+
+
+if __name__ == '__main__':
+    main()

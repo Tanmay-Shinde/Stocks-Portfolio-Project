252ead7a899d86cc419e6ae76fde0fe8d8e17aa7
Tanmay Shinde <tanmayshinde1401@gmail.com>
2025-07-08 01:13:18 -0400

    updated mem_buy_sell_fact table (removed balance col)

--- database/gold/member_buy_sell.py
+++ database/gold/member_buy_sell.py
@@ -39,12 +39,12 @@ def setup(engine):
 
     buysell_df.sort_values(by='date', inplace=True)
 
-    buysell_df['signed_value'] = buysell_df.apply(lambda x: -x['value'] if x['buy_sell'] == 'buy' else x['value'], axis=1)
-    buysell_df['balance'] = buysell_df['signed_value'].cumsum()
+    # buysell_df['signed_value'] = buysell_df.apply(lambda x: -x['value'] if x['buy_sell'] == 'buy' else x['value'], axis=1)
+    # buysell_df['balance'] = buysell_df['signed_value'].cumsum()
 
     buysell_df.drop(columns='signed_value', inplace=True)
 
-    buysell_df = buysell_df[['date', 'member_id', 'symbol', 'buy_sell', 'price', 'qty', 'value', 'balance', 'timestamp']]
+    buysell_df = buysell_df[['date', 'member_id', 'symbol', 'buy_sell', 'price', 'qty', 'value', 'timestamp']]
 
     buysell_df.to_sql('mem_buy_sell_fact', con=engine, schema="gold", if_exists='append', index=False)
 
